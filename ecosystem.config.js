aa4f070f508b39b898c8c0ce1d4f6276d2b3cee1
Gonçalo Correia <57664115+GoncaloNevesCorreia@users.noreply.github.com>
2023-06-07 03:23:07 +0100

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,13 @@ module.exports = {
       name: "discord-music-bot",
       script: "npm",
       args: "run start",
-      ignore_watch: ["node_modules", "servers.json"],
-      watch: ["src"],
+
+      watch: ["../"],
       watch_delay: 1000,
+      ignore_watch: ["node_modules", "servers.json"],
+      watch_options: {
+        followSymlinks: false,
+      },
     },
   ],
 };
