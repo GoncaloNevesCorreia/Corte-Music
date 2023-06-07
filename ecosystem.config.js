ada61a4bb97563ecbc3a0aa8e9f6c953799a3b34
Gonçalo Correia <57664115+GoncaloNevesCorreia@users.noreply.github.com>
2023-06-07 03:12:56 +0100

    Teste

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "npm",
       args: "run start",
       ignore_watch: ["node_modules", "servers.json"],
+      watch: ["src"],
       watch_delay: 1000,
     },
   ],
